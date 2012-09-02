c48ddb25e0147757e2013bd42cabb65ffba540fc
JamesFlood <jDingo@hotmail.com>
2012-09-02 11:27:19 +1000

    Thinbgs

--- Solar Backend/src/solarcaluator/backend/Solar_BackendServlet.java
+++ Solar Backend/src/solarcaluator/backend/Solar_BackendServlet.java
@@ -1,11 +1,20 @@
 package solarcaluator.backend;
 import java.io.IOException;
 import javax.servlet.http.*;
+import com.google.appengine.api.backends.BackendService;
+import com.google.appengine.api.backends.BackendServiceFactory;
 
 @SuppressWarnings("serial")
 public class Solar_BackendServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
+		
+		BackendService backendsApi = BackendServiceFactory.getBackendService();
+
+		// Get the backend handling the current request.
+		String currentBackendName = backendsApi.getCurrentBackend();
+		
+		
 	}
 }
