e1c95d275e5033f202c2515fb469a9118bab8aee
JamesFlood <jDingo@hotmail.com>
2012-09-03 20:24:19 +1000

    nearly Works

--- Solar Backend/src/solarcaluator/backend/House.java
+++ Solar Backend/src/solarcaluator/backend/House.java
@@ -3,17 +3,17 @@ package solarcaluator.backend;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.JsonElement;
+
 public class House {
 
 	private String City = "";
 	private String Country = "";
-	private List<Roof> Roofs;
+	private List<Roof> Roofs = new ArrayList<Roof>();
 	
 	public void setLocation(String City, String Country) {
 		this.City=City;
 		this.Country=Country;
-		
-		Roofs = new ArrayList<Roof>();
 	}
 
 	public String getCity() {
--- Solar Backend/src/solarcaluator/backend/Solar_BackendServlet.java
+++ Solar Backend/src/solarcaluator/backend/Solar_BackendServlet.java
@@ -5,32 +5,42 @@ import java.io.PrintWriter;
 import javax.servlet.http.*;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 @SuppressWarnings("serial")
 public class Solar_BackendServlet extends HttpServlet {
 	
 	private int DAILY = 1;
 	private int ANNUAL = 365;
+	House house = new House();
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		//resp.setContentType("application/json");
-		resp.setContentType("text/html");
+		resp.setContentType("application/json");
+		//resp.setContentType("text/html");
 
-		House house = new Gson().fromJson(req.getParameter("jsonParameter"), House.class);
+		//House house = new Gson().fromJson(req.getParameter("jsonParameter"), House.class);
 		
-		//JSONObject json = (JSONObject)new JsonParser().parse(req.getParameter("jsonParameter"));
+		JsonObject json = (JsonObject)new JsonParser().parse(req.getParameter("jsonParameter"));
+		JsonObject jsonHouse = json.getAsJsonObject("house");
+		
+		house.setLocation(jsonHouse.get("city").toString(), jsonHouse.get("country").toString());
 		
 		double power = CalculatePower.calculate(75,CalculatePower.efficiency(45, "n", 0),7,DAILY);
 		
-		String JSONoutput = "{" +
-				" \"Results\": {" +
-					" \"Daily Power Generation\": \"" + power + "\"," +
-					" \"Annual Power Generation\": \"" + power * ANNUAL + "\" }" +
-				" }";
+//		String JSONoutput = "{" +
+//				" \"Results\": {" +
+//					" \"Daily Power Generation\": \"" + power + "\"," +
+//					" \"Annual Power Generation\": \"" + power * ANNUAL + "\" }" +
+//				" }";
 		
 		PrintWriter out = resp.getWriter();
-		out.println(JSONoutput);
+//		out.println(JSONoutput);
 		out.println(house.getRoof(0).getName());
 		out.println(house.getCity());
+		out.println(req.getParameter("jsonParameter"));
+		System.out.println("Test");
+		System.out.println(req.getParameter("jsonParameter"));
 	}
 }
